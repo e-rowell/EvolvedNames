fd764f324cc8be9721c70217df77ec3c75660c95
nicholas hays <nicholas.hays@hotmail.com>
2016-01-17 17:49:50 -0800

    no more changes

--- EvolvedNames/src/Genome.java
+++ EvolvedNames/src/Genome.java
@@ -1,11 +1,5 @@
 import java.util.Random;
-//make some other sweet awesome changes
-<<<<<<< Upstream, based on origin/master
-//some changes
-//some extra sweet awesome changes
-=======
-//make some other sweet awesome changes
->>>>>>> 65fc9fa made sweet awesome changes dude
+//class genome
 public class Genome {
 	public static Random RANDOM = new Random();
 	public double myMutationRate;
